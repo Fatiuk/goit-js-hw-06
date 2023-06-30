f7b01ffdfdf825c19cbb0cebd2b63284ae2103ce
Fatiuk <7777bmx7777@gmail.com>
2023-06-30 14:53:31 +0300

    Update 0.3
    Complete task 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,5 +1,30 @@
+const createButton = document.querySelector('[data-create]');
+const destroyButton = document.querySelector('[data-destroy]');
+const boxes = document.getElementById('boxes');
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const createBoxes = amount => {
+  for (let i = 0; i < amount; i += 1) {
+    const box = document.createElement('div');
+    box.style.backgroundColor = getRandomHexColor();
+    box.style.width = `${30 + i * 10}px`;
+    box.style.height = `${30 + i * 10}px`;
+    boxes.appendChild(box);
+  }
+  console.log(Number(document.querySelector('input').value));
+};
+
+const destroyBoxes = () => {
+  boxes.innerHTML = '';
+};
+
+createButton.addEventListener('click', () => {
+  const amount = Number(document.querySelector('input').value);
+  createBoxes(amount);
+});
+destroyButton.addEventListener('click', destroyBoxes);
