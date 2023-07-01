b925b287f2e964af5a509d20da33b53d8d5ce6c4
Fatiuk <7777bmx7777@gmail.com>
2023-07-01 14:16:35 +0300

    Update 0.6
    Add trim() for task-06

--- js/task-06.js
+++ js/task-06.js
@@ -2,7 +2,7 @@ const validationInput = document.getElementById('validation-input');
 const validationDataLength = validationInput.getAttribute('data-length');
 
 validationInput.addEventListener('blur', () => {
-  if (validationInput.value.length === Number(validationDataLength)) {
+  if (validationInput.value.trim().length === Number(validationDataLength)) {
     validationInput.classList.add('valid');
     validationInput.classList.remove('invalid');
   } else {
