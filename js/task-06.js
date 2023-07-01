a95cd6b6845f61ab647b0b2da69cbdd418c8ac0a
Fatiuk <7777bmx7777@gmail.com>
2023-07-01 15:08:53 +0300

    Update 0.7
    Update task-06 (new)

--- js/task-06.js
+++ js/task-06.js
@@ -1,12 +1,25 @@
 const validationInput = document.getElementById('validation-input');
 const validationDataLength = validationInput.getAttribute('data-length');
 
-validationInput.addEventListener('blur', () => {
-  if (validationInput.value.trim().length === Number(validationDataLength)) {
-    validationInput.classList.add('valid');
-    validationInput.classList.remove('invalid');
-  } else {
-    validationInput.classList.add('invalid');
-    validationInput.classList.remove('valid');
-  }
-});
+// validationInput.addEventListener('blur', () => {
+//   if (validationInput.value.trim().length === Number(validationDataLength)) {
+//     validationInput.classList.add('valid');
+//     validationInput.classList.remove('invalid');
+//   } else {
+//     validationInput.classList.add('invalid');
+//     validationInput.classList.remove('valid');
+//   }
+// });
+
+validationInput.addEventListener('blur', validateInput);
+
+function validateInput() {
+  validationInput.value.trim().length === Number(validationDataLength)
+    ? changeClass('valid', 'invalid')
+    : changeClass('invalid', 'valid');
+}
+
+function changeClass(firstClass, secondClass) {
+  validationInput.classList.add(firstClass);
+  validationInput.classList.remove(secondClass);
+}
